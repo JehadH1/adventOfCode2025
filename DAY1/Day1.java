3489b3bc18ffe888b7c54db5a8f833d74f0112ef
jehadh1 <jehadh244@gmail.com>
2024-12-01 23:36:44 -0700

    im him

---
+++ DAY1/Day1.java
@@ -0,0 +1,39 @@
+package DAY1;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner input = null;
+        try {
+            input = new Scanner(new File("DAY1/DData1.txt"));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Vector<Integer> L1 = new Vector<>();
+        Vector<Integer> L2 = new Vector<>();
+
+        if (input == null) {
+            return;
+        }
+
+        while (input.hasNextLine()) {
+            L1.add(input.nextInt());
+            L2.add(input.nextInt());
+        }
+
+        Collections.sort(L1);
+        Collections.sort(L2);
+        int num = 0;
+        for(int i = 0; i<L1.size();i++){
+            num += Math.abs((L1.get(i) - L2.get(i)));
+        }
+
+        System.out.println(num);
+    }
+}
