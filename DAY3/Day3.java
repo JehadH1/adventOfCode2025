44d60a02b00571177161b2eb3141496fe481db83
jehadh1 <jehadh244@gmail.com>
2024-12-02 22:16:51 -0700

    start

---
+++ DAY3/Day3.java
@@ -0,0 +1,20 @@
+package DAY3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
+
+public class Day3 {
+    public static void main(String[] args) {
+        Scanner input = null;
+        try {
+             input = new Scanner(new File("DAY3/DData3.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        
+
+        
+    }
+}
